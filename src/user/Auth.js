e495edfcc18265eb577e3b05d4faf3ca62edc9b5
LearnToCode1138 <mdelgado1138@hotmail.com>
2023-11-24 17:25:52 -0800

    notification added

--- src/user/Auth.js
+++ src/user/Auth.js
@@ -8,6 +8,7 @@ import Button from '../shared/UIElements/Button';
 import Modal from '../shared/UIElements/Modal';
 import LoadingSpinner from '../shared/UIElements/LoadingSpinner';
 import FormInput from '../shared/components/FormElements/FormInput';
+import Notification from '../shared/UIElements/Notification';
 import { useForm } from '../shared/hooks/form-hook';
 import { useHttpClient } from '../shared/hooks/http-hook';
 import { authActions } from '../store/auth-slice';
@@ -23,6 +24,8 @@ const Auth = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const { isSubmitting, error, sendRequest, clearError } = useHttpClient();
+  const [showNotification, setShowNotification] = useState(true);
+  const [showNotification2, setShowNotification2] = useState();
   const [isLoginMode, setIsLoginMode] = useState(true);
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -37,11 +40,23 @@ const Auth = () => {
     },
     false
   );
+  const notifyBackend =
+    'Backend services may take a minute to startup---your patience is appreciated!';
+  const notifyLogin =
+    'For an example account with a full list of products, login as user test@address.com, password smithco123 ';
 
   if (isAuth) {
     dispatch(authActions.logout());
     history.push('/auth');
   }
+
+  const notificationHandler = () => {
+    setShowNotification(false);
+    setShowNotification2(true);
+  };
+  const notificationHandler2 = () => {
+    setShowNotification2(false);
+  };
 
   // changes mode between login and signup
   const modeHandler = () => {
@@ -132,73 +147,81 @@ const Auth = () => {
   };
 
   return (
-    <Section>
-      <div className={classes['auth-container']}>
-        <Card width="40">
-          <Modal
-            show={error === undefined || null ? false : true}
-            msgHeader={
-              isLoginMode ? 'Error during login' : 'Error during signup'
-            }
-            onClear={clearError}
-          >
-            {error}
-          </Modal>
-          <h3>{isLoginMode ? 'Please Login' : 'Create an Account'}</h3>
-          <form onSubmit={submitHandler}>
-            {isSubmitting && <LoadingSpinner />}
-            {!isLoginMode && (
-              <>
-                <FormInput
-                  element="input"
-                  id="name"
-                  type="text"
-                  label="Name"
-                  validators={[VALIDATOR_REQUIRE()]}
-                  errorText="Please enter a name"
-                  onInput={inputHandler}
-                />
-                <FormInput
-                  element="input"
-                  id="company"
-                  type="text"
-                  label="Company"
-                  validators={[VALIDATOR_REQUIRE()]}
-                  errorText="Please enter a company name"
-                  onInput={inputHandler}
-                />
-              </>
-            )}
-            <FormInput
-              element="input"
-              id="email"
-              type="email"
-              label="Email"
-              validators={[VALIDATOR_EMAIL()]}
-              errorText="Please enter a valid email address"
-              onInput={inputHandler}
-            />
-            <FormInput
-              element="input"
-              id="password"
-              type="password"
-              label="Password"
-              validators={[VALIDATOR_MINLENGTH(6)]}
-              errorText="Please enter a valid password (minimum 6 characters)"
-              onInput={inputHandler}
-            />
-            <Button type="submit" disabled={!formState.isValid}>
-              {isLoginMode ? 'Login' : 'Sign up'}
-            </Button>
-          </form>
-          <span className={classes.mode} onClick={modeHandler}>
-            {isLoginMode
-              ? 'Signup for an account'
-              : 'Already have an account?  Please login...'}
-          </span>
-        </Card>
-      </div>
-    </Section>
+    <>
+      {showNotification2 && (
+        <Notification onClear={notificationHandler2} message={notifyLogin} />
+      )}
+      {showNotification && (
+        <Notification onClear={notificationHandler} message={notifyBackend} />
+      )}
+      <Section>
+        <div className={classes['auth-container']}>
+          <Card width="40">
+            <Modal
+              show={error === undefined || null ? false : true}
+              msgHeader={
+                isLoginMode ? 'Error during login' : 'Error during signup'
+              }
+              onClear={clearError}
+            >
+              {error}
+            </Modal>
+            <h3>{isLoginMode ? 'Please Login' : 'Create an Account'}</h3>
+            <form onSubmit={submitHandler}>
+              {isSubmitting && <LoadingSpinner />}
+              {!isLoginMode && (
+                <>
+                  <FormInput
+                    element="input"
+                    id="name"
+                    type="text"
+                    label="Name"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a name"
+                    onInput={inputHandler}
+                  />
+                  <FormInput
+                    element="input"
+                    id="company"
+                    type="text"
+                    label="Company"
+                    validators={[VALIDATOR_REQUIRE()]}
+                    errorText="Please enter a company name"
+                    onInput={inputHandler}
+                  />
+                </>
+              )}
+              <FormInput
+                element="input"
+                id="email"
+                type="email"
+                label="Email"
+                validators={[VALIDATOR_EMAIL()]}
+                errorText="Please enter a valid email address"
+                onInput={inputHandler}
+              />
+              <FormInput
+                element="input"
+                id="password"
+                type="password"
+                label="Password"
+                validators={[VALIDATOR_MINLENGTH(6)]}
+                errorText="Please enter a valid password (minimum 6 characters)"
+                onInput={inputHandler}
+              />
+              <Button type="submit" disabled={!formState.isValid}>
+                {isLoginMode ? 'Login' : 'Sign up'}
+              </Button>
+            </form>
+            <span className={classes.mode} onClick={modeHandler}>
+              {isLoginMode
+                ? 'Signup for an account'
+                : 'Already have an account?  Please login...'}
+            </span>
+          </Card>
+        </div>
+      </Section>
+    </>
   );
 };
 
